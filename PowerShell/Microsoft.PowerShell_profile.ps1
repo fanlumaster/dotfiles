9e51c4a34cabbd73fe18ff906dc23579b5979b16
fanlumaster <1730976608@gmail.com>
2024-08-17 11:45:19 +0800

    update pwsh profile

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -6,6 +6,7 @@ Set-Alias -Name cide -Value code-insiders.cmd
 Set-Alias -Name code -Value code.cmd
 Set-Alias -Name nide -Value neovide
 Set-Alias -Name touch -Value New-Item
+Set-Alias -Name reboot -Value Restart-Computer
 $env:HTTP_PROXY="http://127.0.0.1:7890"
 $env:HTTPS_PROXY="http://127.0.0.1:7890"
 $env:NO_PROXY="localhost,127.0.0.1,::1,192.168.0.0/16"
@@ -13,3 +14,35 @@ Invoke-Expression (&starship init powershell)
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 # chcp 65001
 # cls
+Set-PSReadLineOption -EditMode Emacs # Set shortcuts mode
+
+function cdnvim()
+{
+  Set-Location $env:LOCALAPPDATA/nvim
+}
+
+function cdnvimdata()
+{
+  Set-Location $env:LOCALAPPDATA/nvim-data
+}
+
+function gituser()
+{
+  git config user.name "fanlumaster"
+  git config user.email "1730976608@qq.com"
+}
+
+function gitcm($msg)
+{
+  git commit -m "$msg"
+}
+
+function cdcaptures
+{
+  Set-Location "C:\Users\fanyfull\AppData\Roaming\PotPlayerMini64\Capture"
+}
+
+function cdpotplayercaptures
+{
+  Set-Location "C:\Users\fanyfull\AppData\Roaming\PotPlayerMini64\Capture"
+}
