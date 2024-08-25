7bb6c2eb4ca9a8f93f6a3e646d959087812a61bb
fanlumaster <1730976608@qq.com>
2024-08-25 22:27:01 +0800

    update fish

--- fish/alias.fish
+++ fish/alias.fish
@@ -1,6 +1,8 @@
 alias cls="clear"
 alias py="python"
-alias nide="neovide"
+alias nide="neovide --fork"
+alias start="open"
+alias del="rm"
 
 function cdnvim
   cd ~/.config/nvim/
@@ -17,3 +19,37 @@ end
 function gitcm
   git commit -m $argv
 end
+
+function cdhexo
+  cd ~/HDisk/Hexo/fanlumaster
+end
+
+function cdhugo
+  cd ~/HDisk/Hugo/SonnyCalcr
+end
+
+function cdcppprimer
+  cd ~/EDisk/CppCodes/CppPrimer5th/
+end
+
+# compile and run cpp binary files
+function llaunch
+  #/home/fanyfull/EDisk/AlgoCodes/leetcode-records/Cpp/llaunch.sh
+  ./llaunch.sh
+end
+
+# compile cpp binary files
+function lcompile
+  ./lcompile.sh
+end
+
+# compile cpp binary files
+function lrun
+  ./lrun.sh
+end
+
+# use .. to replace cd ../, and so on
+function multicd
+    echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
+end
+abbr --add dotdot --regex '^\.\.+$' --function multicd
--- fish/config.fish
+++ fish/config.fish
@@ -5,30 +5,14 @@ end
 # starship
 starship init fish | source
 
+# make Alt + l to accept current suggestion like using right arrow
+bind \el forward-char
+
 # Aliases
 if [ -f $HOME/.config/fish/alias.fish ]
     source $HOME/.config/fish/alias.fish
 end
 
-# 自定义的补全函数，将 script.sh 补全为 ./script.sh
-complete -c llau -a '(echo bar\nbaz\nbiz)' -f
-
-# compile and run cpp binary files
-function llaunch
-  #/home/fanyfull/EDisk/AlgoCodes/leetcode-records/Cpp/llaunch.sh
-  ./llaunch.sh
+function fish_greeting
+    random choice "Hi Sonny Calcr!"
 end
-
-# compile cpp binary files
-function lcompile
-  ./lcompile.sh
-end
-
-# compile cpp binary files
-function lrun
-  ./lrun.sh
-end
-
-# function gitcm
-#   git 
-# end
