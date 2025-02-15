5591aedfad6e2b6f4740b84aa656435beff968cb
fanlumaster <1730976608@qq.com>
2025-02-15 14:45:45 +0800

    update

--- fish/alias.fish
+++ fish/alias.fish
@@ -5,75 +5,77 @@ alias start="open"
 alias del="rm"
 alias nvimclean="nvim --clean"
 alias nideclean="neovide -- --clean"
+alias ff="fastfetch"
+alias codi="codium"
 
 function changewallpaper
-  ~/.config/swww/swww.sh
+    ~/.config/swww/swww.sh
 end
 
 function gitconfigsonnycalcr
-  git config user.email "sonnycalcr@gmail.com"
-  git config user.name "sonnycalcr"
+    git config user.email "sonnycalcr@gmail.com"
+    git config user.name sonnycalcr
 end
 
 function gitconfigfanlumaster
-  git config user.email "1730976608@qq.com"
-  git config user.name "fanlumaster"
+    git config user.email "1730976608@qq.com"
+    git config user.name fanlumaster
 end
 
 function obwayland
-  /usr/bin/obsidian --no-sandbox %U --enable-features=UseOzonePlatform --ozone-platform=wayland --enable-wayland-ime
+    /usr/bin/obsidian --no-sandbox %U --enable-features=UseOzonePlatform --ozone-platform=wayland --enable-wayland-ime
 end
 
 function cdhypr
-  cd ~/.config/hypr/
+    cd ~/.config/hypr/
 end
 
 function cdnvim
-  cd ~/.config/nvim/
+    cd ~/.config/nvim/
 end
 
 function cdnvimdata
-  cd ~/.local/share/nvim
+    cd ~/.local/share/nvim
 end
 
 function cdfish
-  cd ~/.config/fish/
+    cd ~/.config/fish/
 end
 
 function gitcm
-  git commit -m $argv
+    git commit -m $argv
 end
 
 function cdhexo
-  cd ~/HDisk/Hexo/fanlumaster
+    cd ~/HDisk/Hexo/fanlumaster
 end
 
 function cdhugo
-  cd ~/HDisk/Hugo/SonnyCalcr
+    cd ~/HDisk/Hugo/SonnyCalcr
 end
 
 function cdcppprimer
-  cd ~/EDisk/CppCodes/CppPrimer5th/
+    cd ~/EDisk/CppCodes/CppPrimer5th/
 end
 
 function zfanime
-  cd ~/EDisk/CppCodes/IMECodes/fcitx5-FanIME
+    cd ~/EDisk/CppCodes/IMECodes/fcitx5-FanIME
 end
 
 # compile and run cpp binary files
 function llaunch
-  #/home/fanyfull/EDisk/AlgoCodes/leetcode-records/Cpp/llaunch.sh
-  ./llaunch.sh
+    #/home/fanyfull/EDisk/AlgoCodes/leetcode-records/Cpp/llaunch.sh
+    ./llaunch.sh
 end
 
 # compile cpp binary files
 function lcompile
-  ./lcompile.sh
+    ./lcompile.sh
 end
 
 # compile cpp binary files
 function lrun
-  ./lrun.sh
+    ./lrun.sh
 end
 
 # use .. to replace cd ../, and so on
